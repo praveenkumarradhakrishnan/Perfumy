2238e8919ca47809ed289be351dd562f45b3014b
PRAVEEN KUMAR <114936905+praveenkumarradhakrishnan@users.noreply.github.com>
2025-03-18 10:26:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import NavBar from "./components/Navbar"
+import Product from "./components/Product"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Search from "./components/Search"
+
+function App() {
+    return (
+        <>
+            <NavBar></NavBar>
+            <Search></Search>
+            <Product></Product>
+            <About></About>
+            <Footer></Footer>
+        </>
+    )
+
+
+}
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+
+//Navbar component
+function NavBar() {
+    return (
+        <nav>
+            <h1>Perfumy</h1>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About </li>
+                <li>Contact US</li>
+
+            </ul>
+        </nav>
+    )
+}
+export default NavBar
